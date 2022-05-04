819e4d5796fd7e00ef253a77a49ef8cd2df919dc
palomagarcia14 <101289337+palomagarcia14@users.noreply.github.com>
2022-05-04 17:34:47 -0400

    Add files via upload

--- grid.py
+++ grid.py
@@ -27,19 +27,37 @@ win.flip()
 core.wait(5)
 
 
-#need to include a while statement here that triggers this to repeat when a piece falls
-piece_type = random.randint(1,8)
-if piece_type == 1: #cyan straight horizontal line
-    piece = visual #need to design piece (but don't draw on screen!)
-elif piece_type == 2: #royal blue left L
-    piece = visual
-elif piece_type == 3: #orange right L
-    piece = visual
-elif piece_type == 4: #yellow square
-    piece = visual
-elif piece_type == 5: #green parallelogram-type shape
-    piece = visual
-elif piece_type == 6: #red parallelogram-type shape
-    piece = visual
-else: #purple T
-    piece = visual
+#maybe we should define each piece by the coordinates it will first fill
+    #then we could just do math on the coordinates in response to the keypresses
+
+#this function takes no input and randomly selects a type of tetris piece
+    #it outputs the starting coordinates within the grid of all 4 squares and what color they should be
+def piece_generator
+    piece_type = random.randint(1,8)
+    if piece_type == 1: #cyan straight horizontal line ("I")
+        starting_position = [[0, 3], [0, 4], [0, 5], [0,6]] #starting coordinates of this piece
+        color = [] #cyan
+    elif piece_type == 2: #royal blue left L
+        starting_position = [[1,4], [1,5], [1,6], [0,6]]
+        color = [] #royal blue
+    elif piece_type == 3: #orange right L
+        starting_position = [[1,4], [1,5], [1,6], [0,4]]
+        color = [] #orange
+    elif piece_type == 4: #yellow square
+        starting_position = [[0,4], [1,4], [0,5], [1,5]]
+        color = [] #yellow
+    elif piece_type == 5: #green parallelogram-type shape
+        starting_position = [[1,4], [0,5], [1,5], [0,6]]
+        color = [] #green
+    elif piece_type == 6: #red parallelogram-type shape
+        starting_position = [[0,4], [0,5], [1,5], [1,6]]
+        color = [] #red
+    elif piece_type == 7: #purple T
+        starting_position = [[0,4], [0,5], [1,5], [0,6]]
+        color = [] #purple
+    return stating_position, color
+
+
+
+
+#then for a given amount of time that passes, we can just adjust the coordinates on the piece within the grid
