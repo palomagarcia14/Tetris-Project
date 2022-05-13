0af293d29d79f4f1f5a91bec66d441079c7d584f
hayleygasbarro <101277590+hayleygasbarro@users.noreply.github.com>
2022-05-13 13:37:50 -0400

    Add files via upload

---
+++ letsplaytetris_text.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+from pygame.locals import *
+import time
+from pygame.locals import KEYDOWN, K_q
+
+# Constants
+X = 600
+Y = 600
+SCREENSIZE = X, Y
+PADDING = PADTOPBOTTOM, PADLEFTRIGHT = 100,100
+
+# Color
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+# Initialize pygame
+pygame.init()
+
+# Create screen
+screen=pygame.display.set_mode((X,Y))
+
+# Font
+font = pygame.font.SysFont('freesansbold.ttf', 48)
+tetris_text = font.render("Let's play Tetris!", True, WHITE)
+
+running = True
+background = BLACK
+while running:
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            running = False
+
+    screen.fill(background)
+    screen.blit(tetris_text, (170, 25))
+    pygame.display.update()
+
+pygame.quit()
