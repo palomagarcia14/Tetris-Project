15ef6355bdbe3f518e1fc2b0d922d6fdcb6de1fa
hayleygasbarro <101277590+hayleygasbarro@users.noreply.github.com>
2022-05-13 14:45:12 -0400

    Add files via upload

--- pygame_grid.py
+++ pygame_grid.py
@@ -2,21 +2,32 @@ import sys
 import pygame
 from pygame.locals import KEYDOWN, K_q
 
+pygame.font.init()
 # CONSTANTS:
-SCREENSIZE = WIDTH, HEIGHT = 600, 600
+X = 600
+Y = 600
+SCREENSIZE = X, Y
 BLACK = (0, 0, 0)
 WHITE = (255,255,255)
 PADDING = PADTOPBOTTOM, PADLEFTRIGHT = 100,100
+
+# Creating display surface object
+display_surface = pygame.display.set_mode((X,Y))
+
+# Name the window
+pygame.display.set_caption('TETRIS')
+screen=pygame.display.set_mode((X,Y))
+
 # VARS:
-_VARS = {'surf': False}
+_VARS = {'surface': False}
 
 
 def main():
     pygame.init()
-    _VARS['surf'] = pygame.display.set_mode(SCREENSIZE)
+    _VARS['surface'] = pygame.display.set_mode(SCREENSIZE)
     while True:
         checkEvents()
-        _VARS['surf'].fill(BLACK)
+        _VARS['surface'].fill(BLACK)
         drawGrid(15)
         pygame.display.update()
 
@@ -25,41 +36,40 @@ def drawGrid(divisions):
     # DRAW Rectangle
     # TOP lEFT TO RIGHT
     pygame.draw.line(
-      _VARS['surf'], WHITE,
+      _VARS['surface'], WHITE,
       (0 + PADLEFTRIGHT, 0 + PADTOPBOTTOM),
-      (WIDTH - PADLEFTRIGHT, 0 + PADTOPBOTTOM), 2)
+      (X - PADLEFTRIGHT, 0 + PADTOPBOTTOM), 2)
     # BOTTOM lEFT TO RIGHT
     pygame.draw.line(
-      _VARS['surf'], WHITE,
-      (0 + PADLEFTRIGHT, HEIGHT - PADTOPBOTTOM),
-      (WIDTH - PADLEFTRIGHT, HEIGHT - PADTOPBOTTOM), 2)
+      _VARS['surface'], WHITE,
+      (0 + PADLEFTRIGHT, Y - PADTOPBOTTOM),
+      (X - PADLEFTRIGHT, Y - PADTOPBOTTOM), 2)
     # LEFT TOP TO BOTTOM
     pygame.draw.line(
-      _VARS['surf'], WHITE,
+      _VARS['surface'], WHITE,
       (0 + PADLEFTRIGHT, 0 + PADTOPBOTTOM),
-      (0 + PADLEFTRIGHT, HEIGHT - PADTOPBOTTOM), 2)
+      (0 + PADLEFTRIGHT, Y - PADTOPBOTTOM), 2)
     # RIGHT TOP TO BOTTOM
     pygame.draw.line(
-      _VARS['surf'], WHITE,
-      (WIDTH - PADLEFTRIGHT, 0 + PADTOPBOTTOM),
-      (WIDTH - PADLEFTRIGHT, HEIGHT - PADTOPBOTTOM), 2)
+      _VARS['surface'], WHITE,
+      (X - PADLEFTRIGHT, 0 + PADTOPBOTTOM),
+      (X - PADLEFTRIGHT, Y - PADTOPBOTTOM), 2)
 
     # Get cell size
-    horizontal_cellsize = (WIDTH - (PADLEFTRIGHT*2))/divisions
-    vertical_cellsize = (HEIGHT - (PADTOPBOTTOM*2))/divisions
+    horizontal_cellsize = (X - (PADLEFTRIGHT*2))/divisions
+    vertical_cellsize = (Y - (PADTOPBOTTOM*2))/divisions
 
     # VERTICAL DIVISIONS: (0,1,2) for grid(3) for example
     for x in range(divisions):
         pygame.draw.line(
-           _VARS['surf'], WHITE,
+           _VARS['surface'], WHITE,
            (0 + PADLEFTRIGHT+(horizontal_cellsize*x), 0 + PADTOPBOTTOM),
-           (0 + PADLEFTRIGHT+horizontal_cellsize*x, HEIGHT - PADTOPBOTTOM), 2)
+           (0 + PADLEFTRIGHT+horizontal_cellsize*x, Y - PADTOPBOTTOM), 2)
     # HORITZONTAL DIVISION
         pygame.draw.line(
-          _VARS['surf'], WHITE,
+          _VARS['surface'], WHITE,
           (0 + PADLEFTRIGHT, 0 + PADTOPBOTTOM + (vertical_cellsize*x)),
-          (WIDTH - PADLEFTRIGHT, 0 + PADTOPBOTTOM + (vertical_cellsize*x)), 2)
-
+          (X - PADLEFTRIGHT, 0 + PADTOPBOTTOM + (vertical_cellsize*x)), 2)
 
 def checkEvents():
     for event in pygame.event.get():
@@ -68,7 +78,6 @@ def checkEvents():
         elif event.type == KEYDOWN and event.key == K_q:
             pygame.quit()
             sys.exit()
-
 
 if __name__ == '__main__':
     main()
