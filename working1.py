a59ef3248745aa8eabe8b50198090083e0d7a1e6
20mccoya <77861714+20mccoya@users.noreply.github.com>
2022-05-16 01:01:33 -0400

    Add files via upload

--- working1.py
+++ working1.py
@@ -4,7 +4,7 @@ from pygame.locals import KEYDOWN, K_q
 import time
 import random
 
-
+gameDisplay = pg.display.set_mode((600, 600))
 SCREENSIZE = WIDTH, HEIGHT = 600, 600
 BLACK = (0, 0, 0)
 WHITE = (255,255,255)
@@ -89,7 +89,7 @@ elif randa == 5:
     acolor = [0,255,128]
 canfalla = True
 
-
+current_score = 0
 
 def main():
     global canfall, s1,s2,s3,s4,scolor
@@ -99,12 +99,13 @@ def main():
     while True:
         checkEvents()
         _VARS['surf'].fill(BLACK)
-        drawGrid1()
         drawBounds()
         leftright()
         if canfall == True:
+            drawGrid1()
             squareFall()
         elif canfall == False:
+            drawGrid1()
             plist = [1, 2, 3 ,4,5]
             randi = random.choice(plist)
             if randi == 1: #square
@@ -143,8 +144,10 @@ def main():
             pg.draw.rect(_VARS['surf'], scolor, s4)
             canfall = True
         if canfalla == True:
+            drawGrid1()
             squareFalla()
         elif canfalla == False:
+            drawGrid1()
             alist = [1, 2, 3 ,4,5]
             randa = random.choice(alist)
             if randa == 1: #square
@@ -192,11 +195,19 @@ def main():
 #def sets():
 
 def drawGrid1():
+    global current_score
     for i in range(5, 20):
         for j in range(5, 15):
             pg.draw.rect(_VARS['surf'], color, pg.Rect(j*25, i*25,25,25), 2)
+    pg.draw.rect(_VARS['surf'], (255, 0, 0), pg.Rect(435, 210, 125, 100))
+    pg.font.init()
+    my_font1 = pg.font.SysFont('Comic Sans MS', 50)
+    text_surface = my_font1.render(str(current_score), False, (0, 0, 0))
+    gameDisplay.blit(text_surface, (485,245))
+    my_font2 = pg.font.SysFont('Comic Sans MS', 30)
+    text_surface2 = my_font2.render('Score:', False, (0, 0, 0))
+    gameDisplay.blit(text_surface2, (445,215))
 
-    pg.draw.rect(_VARS['surf'], [255,0,0], lilguy)
     pg.display.flip()
 
 def drawBounds():
@@ -205,10 +216,12 @@ def drawBounds():
     pg.draw.rect(_VARS['surf'], [255,0,255], b2)
     pg.draw.rect(_VARS['surf'], [255,0,255], b3)
     pg.draw.rect(_VARS['surf'], [255,0,255], b4)
+    pg.draw.rect(_VARS['surf'], [255,0,0], lilguy)
 
 
 def squareFall():
     global canfall, s1,s2,s3,s4,scolor
+    global current_score
 
 
     if s3.colliderect(b1) :
@@ -217,6 +230,7 @@ def squareFall():
             pg.draw.rect(_VARS['surf'], scolor, s3)
             pg.draw.rect(_VARS['surf'], scolor, s4)
             canfall = False
+            current_score +=1
 
 
     else:
@@ -233,12 +247,14 @@ def squareFall():
 
 def squareFalla():
     global canfalla, a1,a2,a3,a4,acolor
+    global current_score
     if a3.colliderect(b1):
             pg.draw.rect(_VARS['surf'], scolor, a1)
             pg.draw.rect(_VARS['surf'], scolor, a2)
             pg.draw.rect(_VARS['surf'], scolor, a3)
             pg.draw.rect(_VARS['surf'], scolor, a4)
             canfalla = False
+            current_score +=1
     else:
         d = [0, 25]
         a1.move_ip(d)
@@ -259,6 +275,12 @@ def leftright():
     if key_input[pg.K_RIGHT]:
         c = [step, 0]
         lilguy.move_ip(c)
+    if key_input[pg.K_UP]:
+        y = [0, -step]
+        lilguy.move_ip(y)
+    if key_input[pg.K_DOWN]:
+        z = [0, step]
+        lilguy.move_ip(z)
 
 def gameover():
     global canfalla, a1,a2,a3,a4
