a99af2356ae972113b7c5979d7a2da9e7a27f088
20mccoya <77861714+20mccoya@users.noreply.github.com>
2022-05-16 01:28:24 -0400

    Update nt.py

--- nt.py
+++ nt.py
@@ -16,45 +16,47 @@ _VARS = {'surf': False}
 STARTX = 250
 step = 25
 rect = pg.Rect(STARTX, 100, SQUARE_LEN, SQUARE_WID)
-
+# these longer rectangles were created as the border for the grid so that we could track if the shapes hit the boundaries. 
 b1 = pg.Rect(5*25, 20*25,250,25)
 b2 = pg.Rect(5*25, 4*25,250,25)
 b3 = pg.Rect(4*25, 4*25,25,425)
 b4 = pg.Rect(15*25, 4*25,25,425)
+
+# the following list allowed us to pick out a random integer that corresponded to a particular shape type. 
 plist = [1, 2, 3 ,4,5]
 randi = random.choice(plist)
-if randi == 1: #square
+if randi == 1: #square shape
     s1 = pg.Rect(STARTX, 100, SQUARE_LEN, SQUARE_WID)
     s2 = pg.Rect(STARTX + SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     s3 = pg.Rect(STARTX, 125, SQUARE_LEN, SQUARE_WID)
     s4 = pg.Rect(STARTX + SQUARE_LEN, 125, SQUARE_LEN, SQUARE_WID)
     scolor = [255,255,0]
-elif randi == 2:#line
+elif randi == 2:#line shape
     s1 = pg.Rect(STARTX, 100, SQUARE_LEN, SQUARE_WID)
     s2 = pg.Rect(STARTX + SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     s3 = pg.Rect(STARTX- SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     s4 = pg.Rect(STARTX+ SQUARE_LEN +SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     scolor = [0,255,255]
-elif randi == 3:#w
+elif randi == 3:#w shape
     s1 = pg.Rect(STARTX, 100, SQUARE_LEN, SQUARE_WID)
     s2 = pg.Rect(STARTX + SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     s4 = pg.Rect(STARTX- SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     s3 = pg.Rect(STARTX, 125, SQUARE_LEN, SQUARE_WID)
     scolor = [255,0,255]
-elif randi == 4:
+elif randi == 4:#L shape
     s1 = pg.Rect(STARTX, 100, SQUARE_LEN, SQUARE_WID)
     s2 = pg.Rect(STARTX + SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     s4 = pg.Rect(STARTX- SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     s3 = pg.Rect(STARTX- SQUARE_LEN, 125, SQUARE_LEN, SQUARE_WID)
     scolor = [255, 128, 0]
-elif randi == 5:
+elif randi == 5: #s shape
     s1 = pg.Rect(STARTX, 100, SQUARE_LEN, SQUARE_WID)
     s2 = pg.Rect(STARTX + SQUARE_LEN, 100, SQUARE_LEN, SQUARE_WID)
     s3 = pg.Rect(STARTX- SQUARE_LEN, 125, SQUARE_LEN, SQUARE_WID)
     s4 = pg.Rect(STARTX, 125, SQUARE_LEN, SQUARE_WID)
     scolor = [0,255,128]
 canfall = True
-
+#this current score global variable was created by Paloma 
 current_score = 0
 
 def main():
